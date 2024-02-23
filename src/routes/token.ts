7c6e4f53a285d98f3494d12b8ef678d03e15683b
Erwan Cheriaux <erwan.cheriaux@gmail.com>
2024-02-23 15:04:27 -0800

    rename row api param into col, user choose a column to drop a token, not a row

--- src/routes/token.ts
+++ src/routes/token.ts
@@ -2,7 +2,7 @@ import { Request, Response, Router } from 'express';
 
 const router = Router();
 
-router.put('/user/:row([1-7])', (req: Request, res: Response) => {
+router.put('/user/:col([1-7])', (req: Request, res: Response) => {
     try {
         res.status(200).json('put a token in the given row and return the board');
     } catch (error) {
