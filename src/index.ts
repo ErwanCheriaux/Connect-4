7500ee1cc874981b84cdaa973fb4b17fb5d3dba0
Erwan Cheriaux <erwan.cheriaux@gmail.com>
2024-02-23 11:41:30 -0800

    config morgan to log api traffic and ignore vscode config files

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,15 @@
 import express, { Request, Response, Application } from 'express';
 import dotenv from 'dotenv';
+import morgan from 'morgan';
 
 //For env File
 dotenv.config();
 
 const app: Application = express();
 const port = process.env.PORT || 8000;
+
+// Install middleware
+app.use(morgan('tiny'));
 
 app.get('/', (req: Request, res: Response) => {
     res.send('Welcome to Express & TypeScript Server');
