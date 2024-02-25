a14f43f49d461ab1907ab898634e5b945c41ec65
Erwan Cheriaux <erwan.cheriaux@gmail.com>
2024-02-25 11:32:59 -0800

    add hasConnect4 method to find the winner based on the current move and the player

--- src/classes/Game.ts
+++ src/classes/Game.ts
@@ -1,3 +1,6 @@
+const BOARD_SIZE_COL = 7;
+const BOARD_SIZE_ROW = 6;
+
 enum BoardItem {
     EMPTY = 0,
     USER = 1,
@@ -28,9 +31,9 @@ class Game {
 
     public newGame(): void {
         this.status = GameStatus.NEW;
-        this.board = Array(6)
+        this.board = Array(BOARD_SIZE_ROW)
             .fill(null)
-            .map(() => Array(7).fill(BoardItem.EMPTY));
+            .map(() => Array(BOARD_SIZE_COL).fill(BoardItem.EMPTY));
     }
 
     public getStatus(): GameStatus {
@@ -46,10 +49,18 @@ class Game {
             throw new Error('Column full already!');
         }
 
-        for (let row = 5; row >= 0; row--) {
+        for (let row = BOARD_SIZE_ROW - 1; row >= 0; row--) {
             if (!this.board[row][col]) {
+                //play
                 this.board[row][col] = BoardItem.USER;
-                this.status = GameStatus.HOUSE;
+
+                // check
+                if (this.hasConnect4(row, col, BoardItem.USER)) {
+                    this.status = GameStatus.WIN;
+                } else {
+                    this.status = GameStatus.HOUSE;
+                }
+
                 return;
             }
         }
@@ -61,7 +72,7 @@ class Game {
         }
 
         const notFullcolumns: number[] = [];
-        for (let col = 0; col < 7; col++) {
+        for (let col = 0; col < BOARD_SIZE_COL; col++) {
             if (!this.board[0][col]) notFullcolumns.push(col);
         }
 
@@ -72,10 +83,18 @@ class Game {
         const index: number = Math.floor(Math.random() * notFullcolumns.length);
         const col: number = notFullcolumns[index];
 
-        for (let row = 6; row--; row > 0) {
+        for (let row = BOARD_SIZE_ROW - 1; row >= 0; row--) {
             if (!this.board[row][col]) {
+                // play
                 this.board[row][col] = BoardItem.HOUSE;
-                this.status = GameStatus.USER;
+
+                // check
+                if (this.hasConnect4(row, col, BoardItem.HOUSE)) {
+                    this.status = GameStatus.LOSE;
+                } else {
+                    this.status = GameStatus.USER;
+                }
+
                 return;
             }
         }
@@ -92,6 +111,63 @@ class Game {
         });
 
         return screen;
+    }
+
+    private hasConnect4(row: number, col: number, player: BoardItem): boolean {
+        let line: string;
+        let rowIndex: number;
+        let colIndex: number;
+
+        // horizontal
+        line = '';
+        for (let index = 0; index < BOARD_SIZE_COL; index++) {
+            line += this.board[row][index].toString();
+        }
+
+        if (line.includes((player * 1111).toString())) return true;
+
+        // vertical
+        line = '';
+        for (let index = 0; index < BOARD_SIZE_ROW; index++) {
+            line += this.board[index][col].toString();
+        }
+
+        if (line.includes((player * 1111).toString())) return true;
+
+        // diagonal left-up to right-down
+        line = '';
+        rowIndex = row;
+        colIndex = col;
+
+        while (rowIndex > 0 && colIndex > 0) {
+            rowIndex--;
+            colIndex--;
+        }
+
+        while (rowIndex < BOARD_SIZE_ROW && colIndex < BOARD_SIZE_COL) {
+            line += this.board[rowIndex++][colIndex++].toString();
+        }
+
+        if (line.includes((player * 1111).toString())) return true;
+
+        // diagonal left-down to right-up
+        line = '';
+        rowIndex = row;
+        colIndex = col;
+
+        while (rowIndex < BOARD_SIZE_ROW - 1 && colIndex > 0) {
+            rowIndex++;
+            colIndex--;
+        }
+
+        while (rowIndex > 0 && colIndex < BOARD_SIZE_COL) {
+            line += this.board[rowIndex--][colIndex++].toString();
+        }
+
+        if (line.includes((player * 1111).toString())) return true;
+
+        // no connect 4 detected
+        return false;
     }
 }
 
