4e8e730e30963c78a103dbfade68bc2d939e8e31
Erwan Cheriaux <erwan.cheriaux@gmail.com>
2024-02-27 13:32:11 -0800

    test all api endpoints

--- tests/routes.spec.ts
+++ tests/routes.spec.ts
@@ -6,11 +6,43 @@ describe('Connect4 api endoints', () => {
         server.close(done);
     });
 
-    it('tests /game endpoints', async () => {
+    it('tests POST /game endpoint', async () => {
         const response = await request(app).post('/game');
         expect(response.statusCode).toBe(200);
         expect(response.text).toBe(
             '| | | | | | | |\n| | | | | | | |\n| | | | | | | |\n| | | | | | | |\n| | | | | | | |\n| | | | | | | |\n',
         );
+    });
+
+    it('tests GET /board endpoint', async () => {
+        const response = await request(app).get('/board');
+        expect(response.statusCode).toBe(200);
+        expect(response.text).toHaveLength(96);
+        expect(typeof response.text).toBe('string');
+    });
+
+    it('tests PUT /token/user/1 endpoint, should return board', async () => {
+        const response = await request(app).put('/token/user/1');
+        expect(response.statusCode).toBe(200);
+        expect(response.text).toHaveLength(96);
+        expect(typeof response.text).toBe('string');
+    });
+
+    it('tests PUT /token/user/0 endpoint, should return an error 404', async () => {
+        const response = await request(app).put('/token/user/0');
+        expect(response.statusCode).toBe(404);
+    });
+
+    it('tests PUT /token/house endpoint', async () => {
+        const response = await request(app).put('/token/house');
+        expect(response.statusCode).toBe(200);
+        expect(response.text).toHaveLength(96);
+        expect(typeof response.text).toBe('string');
+    });
+
+    it('tests POST /board/open endpoint', async () => {
+        const response = await request(app).post('/door/open');
+        expect(response.statusCode).toBe(403);
+        expect(response.text).toBe('The door is closed.');
     });
 });
