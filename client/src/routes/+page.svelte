d391e440c99d5db5ca8fcfbc8bdb8fb6d81d542d
Erwan Cheriaux <erwan.cheriaux@gmail.com>
2024-02-27 16:05:12 -0800

    page display the board and new game button

--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -1,2 +1,30 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+    import { onMount } from 'svelte';
+
+    const api = import.meta.env.API_URI || 'http://localhost:8000';
+
+    let screen: string = '';
+
+    const handleNewGame = async () => {
+        try {
+            const response = await fetch(`${api}/game`, { method: 'POST' });
+
+            if (!response.ok) {
+                const error = await response.json();
+                console.error('Error starting a new game:', error);
+            }
+
+            screen = await response.text();
+        } catch (error: unknown) {
+            console.error('Error starting a new game:', error);
+        }
+    };
+
+    onMount(handleNewGame);
+</script>
+
+<h1>Welcome to your door security interface</h1>
+<p>You must win Connect4 to open the door... good luck!</p>
+<pre>{screen}</pre>
+
+<button on:click={handleNewGame}>New game</button>
